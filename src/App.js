b81a1924ed397cee1bcbb1b2a07cce520c62aea6
DmitryDor <dmitrydoroshenko80@gmail.com>
2020-05-01 15:34:54 +0300

    Lesson6(Components)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+
+            <Header/>
+            <Technologes/ >
+
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href=' #S'> Home </a>
+            <a href=' #S'> News Feed </a>
+            <a href=' #S'> Messages </a>
+
+        </div>
+    );
+}
+const Technologes = () => {
+    return (
+        <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
+    );
 }
 
 export default App;
