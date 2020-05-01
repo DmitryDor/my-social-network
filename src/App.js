84cfe1573a60c3f60d38d7d5b37148426612eb5b
DmitryDor <dmitrydoroshenko80@gmail.com>
2020-05-01 21:21:52 +0300

    Lesson11(social network/grid)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,56 @@
 import React from 'react';
 import './App.css';
 
+
 const App = () => {
     return (
-        <div>
-
-            <Header/>
-            <Technologes/ >
-
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src='https://avatars.mds.yandex.net/get-pdb/933338/94068ff5-61aa-43bd-909a-a47aade07a8b/s1200?webp=false'/>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img src="https://www.w3schools.com/howto/img_snow_wide.jpg"/>
+                </div>
+                <div>
+                    ava+discription
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New posts
+                    </div>
+                    <div>
+                        <div>
+                            post1
+                        </div>
+                        <div>
+                            post2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
 
-const Header = () => {
-    return (
-        <div>
-            <a href=' #S'> Home </a>
-            <a href=' #S'> News Feed </a>
-            <a href=' #S'> Messages </a>
-
-        </div>
-    );
-}
-const Technologes = () => {
-    return (
-        <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>js</li>
-            <li>react</li>
-        </ul>
-    );
-}
 
 export default App;
